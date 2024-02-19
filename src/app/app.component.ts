51b7db6b49476819b8e22e9973bfdbb2c1df2019
linuxpedro <pdro.dev97@gmail.com>
2024-02-19 15:39:00 -0300

    testing deferrable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,22 @@ import { NewComponent } from './components/new/new.component';
 import { BindingComponent } from './components/template/binding/binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { ControlFlowComponent } from './components/template/control-flow/control-flow.component';
+import { DeferrableViewsComponent } from './components/template/deferrable-views/deferrable-views.component';
+import { SignalsComponent } from './components/signals/signals/signals.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, BindingComponent, TemplateVariablesComponent, ControlFlowComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NewComponent,
+    BindingComponent,
+    TemplateVariablesComponent,
+    ControlFlowComponent,
+    DeferrableViewsComponent,
+    SignalsComponent,
+  ],
   template: `
     <h1>Curso Angular</h1>
   <!-- 
@@ -19,7 +30,9 @@ import { ControlFlowComponent } from './components/template/control-flow/control
   <app-binding></app-binding>
   <app-template-variables />
   <app-control-flow/>
+  <app-deferrable-views/>
   -->
+  <app-signals/>
   `,
 })
 export class AppComponent {
--- src/app/components/template/deferrable-views/deferrable-views.component.ts
+++ src/app/components/template/deferrable-views/deferrable-views.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NewComponent } from '../../new/new.component';
 
 @Component({
   selector: 'app-deferrable-views',
   standalone: true,
-  imports: [],
+  imports: [NewComponent],
   templateUrl: './deferrable-views.component.html',
   styleUrl: './deferrable-views.component.scss'
 })
 export class DeferrableViewsComponent {
-
+  public isTrue = false
 }
